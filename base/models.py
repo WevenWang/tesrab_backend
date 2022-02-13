82fbd3b7f08fc5ce4e135cd9e9ac6f1bc3e60538
YifanWang97 <yifan@lobbie.com>
2022-02-13 15:12:40 -0800

    finish database modeling and image upload handling

--- base/models.py
+++ base/models.py
@@ -1,3 +1,89 @@
+from itertools import product
+from statistics import mode
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Product(models.Model):
+    # user to product is one to many relationship
+    # do not delete the child element if the parent gets deleted
+    # null = True mean product can be created without a user
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True);
+    name = models.CharField(max_length=200, null=True, blank=True)
+    # use Pillow to process images
+    image = models.ImageField(null=True, blank=True)
+    brand = models.CharField(max_length=200, null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    rating = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    # model = models.CharField(max_length=200, null=True, blank=True)
+    numReviews = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    countInStock = models.IntegerField(null=True, blank=True, default=0)
+    # take a snapshot of when the product is first created
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    # display product name in the admin panel
+    def __str__(self) -> str:
+        return self.name
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    rating = models.IntegerField(null=True, blank=True, default=0)
+    comment = models.TextField(null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return str(self.rating)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    paymentMethod = models.CharField(max_length=200, null=True, blank=True)
+    taxPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    shippingPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    totalPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    isPaid = models.BooleanField(default=False)
+    paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    deliveredAt = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return str(self.createdAt)
+
+class OrderItem(models.Model):
+    # foreign key means many to one relationship
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    qty = models.IntegerField(null=True, blank=True, default=0)
+    price = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    image = models.CharField(max_length=200, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.name
+
+class ShippingAddress(models.Model):
+    # this needs to be one to one relationship
+    # when someone delete the order, the shipping address should be removed (CASCADE)
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, null=True, blank=True) 
+    address = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    postalCode = models.CharField(max_length=200, null=True, blank=True)
+    country = models.CharField(max_length=200, null=True, blank=True)
+    shippingPrice = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    _id = models.AutoField(primary_key=True, editable=False)
+
+    def __str__(self) -> str:
+        return self.address
