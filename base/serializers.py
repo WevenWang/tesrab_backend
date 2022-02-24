4bb4d70441b3b83fd43e3bf1ce3e793e2a01406e
YifanWang97 <yifan@lobbie.com>
2022-02-23 17:56:08 -0800

    add order items using orderm shipping, update stock, create serializers for frontend response

--- base/serializers.py
+++ base/serializers.py
@@ -1,14 +1,56 @@
 from dataclasses import fields
+from nis import cat
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth.models import User
-from .models import Product
+from .models import Product, Order, OrderItem, ShippingAddress
 
 # serializer that wraps around the Product model, turn it into Json format
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
+
+
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShippingAddress
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+# only use orderSerializer in view and nest other in orderSerializer
+class OrderSerializer(serializers.ModelSerializer):
+    
+    orders = serializers.SerializerMethodField(read_only=True)
+    shipping = serializers.SerializerMethodField(read_only=True)
+    user = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = Order
+        fields = '__all__'
+    # when got the order, make queries for child and parent elements
+    # return as nested objects
+    def get_orders(self,obj):
+        items = obj.orderitem_set.all()
+        serializer = OrderItemSerializer(items, many=True)
+        return serializer.data
+    
+    def get_shippingAddress(self, obj):
+        try:
+            address = ShippingAddressSerializer(obj.shippingAddress, many=False)
+        except:
+            address = False
+        return address
+    
+    def get_users(self, obj):
+        user = obj.user
+        serializer = UserSerializer(user, many=False)
+        return serializer.data
+
 
 # serializer that wraps around the User model, turn it into Json format
 class UserSerializer(serializers.ModelSerializer):
--- base/views/order_views.py
+++ base/views/order_views.py
@@ -1,17 +1,67 @@
-# from email import message
-# from wsgiref.util import request_uri
-# from django.shortcuts import render
-# from django.http import JsonResponse
-# from rest_framework.decorators import api_view, permission_classes
-# from rest_framework.permissions import IsAuthenticated, IsAdminUser
-# from rest_framework.response import Response
-# from .products import products
-# from .models import Product
-# from .serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
+from email import message
+from wsgiref.util import request_uri
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from base.products import products
+from base.models import Product, Order, OrderItem, ShippingAddress
+from base.serializers import ProductSerializer, UserSerializer, UserSerializerWithToken,OrderSerializer
 
-# from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-# from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# from django.contrib.auth.models import User
-# from django.contrib.auth.hashers import make_password
-# from rest_framework import status
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
+@api_view(['POST'])
+@permission_classes(['isAuthenticated'])
+def addOrderItems(request):
+
+    # get user data
+    user = request.user
+    data = request.data
+
+    orderItems = data['orderItems']
+
+    # check if there is orderitems to process
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail': 'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+
+        # 1. create order
+        order = Order.objects.create(
+            user = user,
+            paymentMethod = data['paymentMethod'],
+            taxPrice=data['taxPrice'],
+            shippingPrince= data['shippingPrice'],
+            totalPrince=data['totalPrice'],
+        )
+
+        # 2. create shipping address
+        shipping = ShippingAddress.object.create(
+            order=order,
+            address=data['shippingAddress']['address'],
+            city=data['shippingAddress']['city'],
+            postalCode=data['shippingAddress']['postalCode'],
+            country=data['shippingAddress']['country'],
+        )
+        # 3. create order items and set order to orderItem realtionship
+        for i in orderItems:
+            product = Product.objects.get(_id=i['product']) #product attribute has the id value
+            item = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=i['qty'],
+                price=i['price'],
+                image=product.image.url,
+            )
+
+        # 4. update stock
+            product.countInStock -= item.qty
+            product.save()
+    serializer = OrderSerializer(order, many=False)
+    return Response(serializer.data)
